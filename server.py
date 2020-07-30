9a33616f7f9901e318c4f0dc2a3c8bd508ecd8be
parthsujalshah <parthsujalshah@gmail.com>
2020-07-30 14:50:09 +0530

    basic setup

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from ask_sdk_core.skill_builder import SkillBuilder
+from flask_ask_sdk.skill_adapter import SkillAdapter
+from .hello_world import sb
+
+app = Flask(__name__)
+
+skill_adapter = SkillAdapter(skill=sb.create(), skill_id=1, app=app)
+
+@app.route("/")
+def invoke_skill():
+    return skill_adapter.dispatch_request()
