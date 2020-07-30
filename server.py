edfe206d7e35cc0d5196da1c6684cf74d06929ce
parthsujalshah <parthsujalshah@gmail.com>
2020-07-30 16:17:24 +0530

    one file

--- server.py
+++ server.py
@@ -1,20 +1,79 @@
 from flask import Flask
+from ask_sdk_core.skill_builder import SkillBuilder
 from flask_ask_sdk.skill_adapter import SkillAdapter
-from hello_world import sb
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model import Response
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+
+sb = SkillBuilder()
+class LaunchRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            False)
+        return handler_input.response_builder.response
+
+class HelloWorldIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("HelloWorldIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Hello World"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            True)
+        return handler_input.response_builder.response
+
+class AllExceptionHandler(AbstractExceptionHandler):
+
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        # Log the exception in CloudWatch Logs
+        print(exception)
+
+        speech = "Sorry, I didn't get it. Can you please say it again!!"
+        handler_input.response_builder.speak(speech).ask(speech)
+        return handler_input.response_builder.response
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        # any cleanup logic goes here
+
+        return handler_input.response_builder.response
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(HelloWorldIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_exception_handler(AllExceptionHandler())
 
 app = Flask(__name__)
+skill_response = SkillAdapter(
+    skill=sb.create(), skill_id='amzn1.ask.skill.41125c2b-0229-4ff4-8ea4-5290ad3830ea', app=app)
 
-skill_adapter = SkillAdapter(skill=sb.create(), skill_id='amzn1.ask.skill.41125c2b-0229-4ff4-8ea4-5290ad3830ea', app=app)
+skill_response.register(app=app, route="/")
 
-# @app.route("/", methods=['GET', 'POST'])
-# def invoke_skill():
-#     return skill_adapter.dispatch_request()
-
-skill_adapter.register(app=app, route="/")
-
-@app.route("/test", methods=['GET', 'POST'])
-def test():
-    return 'hello world'
-
-if __name__ == "__main__":
-    app.run(debug=True, port='443')
+if __name__ == '__main__':
+    app.run()
