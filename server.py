e312bc93e94659b677faef3c26d391d9b0bbd464
parthsujalshah <parthsujalshah@gmail.com>
2020-07-30 16:10:17 +0530

    register

--- server.py
+++ server.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 skill_adapter = SkillAdapter(skill=sb.create(), skill_id='amzn1.ask.skill.41125c2b-0229-4ff4-8ea4-5290ad3830ea', app=app)
 
-@app.route("/", methods=['GET', 'POST'])
-def invoke_skill():
-    return skill_adapter.dispatch_request()
+# @app.route("/", methods=['GET', 'POST'])
+# def invoke_skill():
+#     return skill_adapter.dispatch_request()
+
+skill_response.register(app=app, route="/")
 
 @app.route("/test", methods=['GET', 'POST'])
 def test():
