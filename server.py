03171ea01d84804359db06ae5b97157c2f30ef9f
parthsujalshah <parthsujalshah@gmail.com>
2020-07-30 15:01:46 +0530

    parent package import

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from ask_sdk_core.skill_builder import SkillBuilder
 from flask_ask_sdk.skill_adapter import SkillAdapter
-from .hello_world import sb
+from hello_world import sb
 
 app = Flask(__name__)
 
